13cc0dca41d380f7e62e4dba6887dff336782e86
dynamicy <fermat.little.theorem@gmail.com>
2018-03-29 12:00:26 +0800

    First commit.

---
+++ MVP/MVP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVP
+//
+//  Created by Chris Huang 黃信文 (奧圖碼) on 29/03/2018.
+//  Copyright © 2018 chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
