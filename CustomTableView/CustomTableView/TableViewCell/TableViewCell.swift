c5b22ddb540a0c9e0f7598ec656836cb581a3057
dynamicy <fermat.little.theorem@gmail.com>
2018-04-12 09:41:19 +0800

    Fix an issue about fetching the wrong xib resource.

--- CustomTableView/CustomTableView/TableViewCell/TableViewCell.swift
+++ CustomTableView/CustomTableView/TableViewCell/TableViewCell.swift
@@ -9,16 +9,8 @@
 import UIKit
 
 class TableViewCell : UITableViewCell {
-
+    
     @IBOutlet weak var cellImage : UIImageView!
     
     @IBOutlet weak var cellLabel : UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-    
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
 }
