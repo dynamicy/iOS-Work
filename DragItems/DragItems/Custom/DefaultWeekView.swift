b74de89ed0b8d1d1161cff73c228eaf4ea714f0c
Chris <chrissw.huang@optoma.com>
2018-11-28 18:31:28 +0800

    Add podfile.

---
+++ DragItems/DragItems/Custom/DefaultWeekView.swift
@@ -0,0 +1,8 @@
+//
+//  DefaultWeekView.swift
+//  DragItems
+//
+//  Created by Chris Huang 黃信文 (奧圖碼) on 2018/11/28.
+//
+
+import Foundation
---
+++ DragItems/DragItems/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DragItems
+//
+//  Created by Chris Huang 黃信文 (奧圖碼) on 2018/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
